aa5213f4e7a096bf5292f5d037a4fd39ccdecc30
samgnz <120472091+samgnz@users.noreply.github.com>
2025-05-26 14:09:44 -0300

    Create tp.py

---
+++ tp.py
@@ -0,0 +1,24 @@
+#dnis
+dni1 = "39115153"
+dni2 = "37125154"
+dni3 = "38115158"
+dni4 = "34116164"
+
+def digitos_unicos(dni):
+    digitos_unicos = []
+    
+    for digito in dni:
+        digito_int = int(digito)
+        if digito_int not in digitos_unicos:
+            digitos_unicos.append(digito_int)
+    return digitos_unicos
+
+
+conjunto1 = digitos_unicos(dni1)
+conjunto2 = digitos_unicos(dni2)
+conjunto3 = digitos_unicos(dni3)
+conjunto4 = digitos_unicos(dni4)
+
+print(conjunto1,conjunto2,conjunto3,conjunto4)
+
+#operaciones
